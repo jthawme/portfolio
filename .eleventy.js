559621d173a8a8a96fc42480c05d9554fe5bf533
Jonathan Thaw <hi@jthaw.me>
2019-09-05 22:40:58 +0100

    Lets work in the open

---
+++ .eleventy.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+    dir: {
+        input: 'views',
+        includes: "../_includes",
+        layouts: "../_layouts",
+        output: '../dist'
+    }
+};
